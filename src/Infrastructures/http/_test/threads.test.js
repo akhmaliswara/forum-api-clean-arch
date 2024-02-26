69c01100cb07c3df62f0537f5cf85b21e28070b8
Akhmal Iswara Adjie <x901277@matahari.com>
2024-02-26 17:42:45 +0700

    revert wrong test

--- src/Infrastructures/http/_test/threads.test.js
+++ src/Infrastructures/http/_test/threads.test.js
@@ -112,7 +112,6 @@ describe('/threads endpoint', () => {
       expect(response.statusCode).toEqual(200)
       expect(responseJson.status).toEqual('success')
       expect(responseJson.data.thread).toBeDefined()
-      expect(1).toEqual(3)
     })
   })
 })
